de0c8e3337bffbde06c3552def423177507a8a6e
Lyailya <meytopintheworld@gmail.com>
2024-01-20 00:39:58 +0600

    Python Variables Exercises

--- 1.py
+++ 1.py
@@ -1,25 +1,14 @@
-x = "awesome"
+carname = "Volvo"
+x = 50
+x = 5
+y = 10
+print(x+y)
+x = 5 
+y = 10
+z = x+y
+print(z)
+x,y,z="Orange", "Banana", "Cherry"
+x=y=z="Orange"
 def myfunc():
-    print("Python is " + x)
-myfunc()
-a= "awesome"
-print("\n")
-def myfunc2():
-    a="fantastic"
-    print("Python is " + a)
-myfunc2()
-print("Python is " + a)
-print("\n")
-#The Global Keyword
-def myfunc3():
-    global c
-    c = "fantastic"
-myfunc3()
-print("Python is " + c)
-print("\n")
-t = "awesome"
-def myfunc4():
-    global t 
-    t = "fantastic"
-myfunc4()
-print("Python is " + t)
+    global x
+    x= "fantastic"
