4fea530056c26583ac9fb1968f5cc08973ad7e9c
Lyailya <meytopintheworld@gmail.com>
2024-01-19 23:57:46 +0600

    Python Variables(1)

--- 1.py
+++ 1.py
@@ -1,15 +1,4 @@
-#this is a comment
-print("Hello world!")
-print("Hello world!") #this a comment
-#print("Hello world!")
-print("Cheers, Mate!")
-#This is a comment
-#written in
-#more than just one line
-print("Hello word!")
-"""
-This is a comment 
-written in
-more than just one line
-"""
-print("Hello world!")
+x =5
+y ="John"
+print(x)
+print(y)
