7aca4c060a4d29a1cd0f173cd26e5b8f3dc1973e
Lyailya <meytopintheworld@gmail.com>
2024-01-20 00:13:20 +0600

    Python Variables Names

--- 1.py
+++ 1.py
@@ -1,3 +1,18 @@
-a =4 
-A="Sally"
-#A will not overwrite a
+myvar = "John"
+my_var="John"
+_my_var="John"
+myVar="John"
+MYVAR="John"
+myvar2="John"
+"""
+Illegial:
+2myvar="John"
+my-var="John
+my var="John"
+Camel Case:
+myVariableName="John"
+Pascal Case:
+MyVariableName="John"
+Snake Case:
+my_variable_name="John"
+"""
