e5bec3e8cb30edca8370f797111654bec2032a15
Lyailya <meytopintheworld@gmail.com>
2024-01-20 00:16:40 +0600

    Python Variables Assign Multiple Values

--- 1.py
+++ 1.py
@@ -1,18 +1,16 @@
-myvar = "John"
-my_var="John"
-_my_var="John"
-myVar="John"
-MYVAR="John"
-myvar2="John"
-"""
-Illegial:
-2myvar="John"
-my-var="John
-my var="John"
-Camel Case:
-myVariableName="John"
-Pascal Case:
-MyVariableName="John"
-Snake Case:
-my_variable_name="John"
-"""
+#Many Values to Multiple Variables
+x,y,z="Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#One Value to Multiple Variables
+a=b=c="Orange"
+print(a)
+print(b)
+print(c)
+#Unpack a Collection
+fruits = ["apples", "bananas", "cherry"]
+q,w,z=fruits
+print(q)
+print(w)
+print(z)
