d846fb423d79bfbb7e575352362f75310c799c8a
Lyailya <meytopintheworld@gmail.com>
2024-01-21 02:23:53 +0600

    fikes

---
+++ Python_Syntax.py
@@ -0,0 +1,5 @@
+#ex1
+print("Hello World")
+#ex 2
+if 5>2:
+    print("YES")
---
+++ Python_Variables.py
@@ -0,0 +1,21 @@
+#ex1
+carname="Volvo"
+#ex 2
+x =50
+#ex 3
+x =5
+y=10
+print(x+y)
+#ex 4
+x=5
+y=10
+z=x+y 
+print(z)
+#ex 5
+x,y,z="Orange","banana","cherry"
+#ex 6
+x=y=z="orange"
+#ex 7
+def myfunc():
+    global x
+    x="fantastic"
