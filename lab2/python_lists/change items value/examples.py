019f2a0f86644098d141048aff2d224e5b199908
Lyailya <meytopintheworld@gmail.com>
2024-01-25 02:10:40 +0600

    added examples

---
+++ lab2/python_lists/change items value/examples.py
@@ -0,0 +1,29 @@
+#example 1
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+#example 2 #the 3 is not included
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+#example 3
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+#example 4
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+#example 5
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+#example 6
+
+
+#example 7
